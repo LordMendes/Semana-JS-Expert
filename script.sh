f761bdb4c0da88f9883c7bfddf452329aa6893ec
Lucas Mendes <lucasmendes.lx@gmail.com>
2020-10-28 01:49:24 -0300

    multiple rendering add to script

--- script.sh
+++ script.sh
@@ -10,20 +10,38 @@ for mediaFile in `ls $ASSETSFOLDER | grep .mp4`; do
   OUTPUT=$ASSETSFOLDER/$FILENAME/$FILENAME
   DURATION=$(ffprobe -i $INPUT -show_format -v quiet | sed -n 's/duration=//p')
 
-  output144=$OUTPUT-$DURATION-144
-  output360=$OUTPUT-$DURATION-360
-  output720=$OUTPUT-$DURATION-720
+  OUTPUT144=$OUTPUT-$DURATION-144
+  OUTPUT360=$OUTPUT-$DURATION-360
+  OUTPUT720=$OUTPUT-$DURATION-720
 
- echo 'rendering in 720p'
+
+ echo 'rendering in 360p'
   ffmpeg -y -i $INPUT \
       -c:a aac -ac 2 \
       -vcodec h264 -acodec aac \
       -ab 128k \
       -movflags frag_keyframe+empty_moov+default_base_moof \
-      -b:v 1500k \
-      -maxrate 1500k \
-      -bufsize 1000k \
-      -vf "scale=-1:720" \
+      -b:v 400k \
+      -maxrate 400k \
+      -bufsize 400k \
+      -vf "scale=-1:360" \
       -v quiet \
-      $OUTPUT720.mp4
+      $OUTPUT360.mp4
+
+ echo 'rendering in 144p'
+  ffmpeg -y -i $INPUT \
+      -c:a aac -ac 2 \
+      -vcodec h264 -acodec aac \
+      -ab 128k \
+      -movflags frag_keyframe+empty_moov+default_base_moof \
+      -b:v 300k \
+      -maxrate 300k \
+      -bufsize 300k \
+      -vf "scale=256:144" \
+      -v quiet \
+      $OUTPUT144.mp4
+
+  echo $OUTPUT144.mp4
+  echo $OUTPUT360.mp4
+  echo $OUTPUT720.mp4
 done
